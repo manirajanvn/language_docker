bfdea507a09855014a53104b6a55aece078c75fc
unknown <manirajan.vn@gmail.com>
2022-08-24 17:10:15 +0530

    code change

--- app/model/model.py
+++ app/model/model.py
@@ -2,12 +2,12 @@ import pickle
 import re
 from pathlib import Path
 
-__version__ = "0.1.2"
+__version__ = "0.1.0"
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
 
 
-with open(f"{BASE_DIR}/trained_pipeline-{__version__}.pkl", "rb") as f:
+with open(f"{BASE_DIR}/trained_pipeline-0.1.0.pkl", "rb") as f:
     model = pickle.load(f)
 
 
